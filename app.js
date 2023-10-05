55577fcbc3f075ddf22d50ef7ac91a445bc653ee
limyeeun <ifg.035@daum.net>
2023-10-05 12:59:09 +0900

    header 수정중

--- app.js
+++ app.js
@@ -2,10 +2,27 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// view template engine
+app.set("view engine", "pug");
+app.set("views", __dirname+"/views");
+
+app.use(express.static('public'));
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.render("home");
+});
+
+app.get('/about', (req, res) => {
+  res.render("about");
+});
+
+app.get('/portfolio', (req, res) => {
+  res.render("portfolio");
+});
+
+app.get('/contact', (req, res)=>{
+    res.render("contact"); //help.pug 찾아서 서버에서 렌더링해라!
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
